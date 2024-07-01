3ea56145724307db32a0adc6bd83ee0b573b2517
Yokereba <kozottio1007@gmail.com>
2024-07-01 15:14:27 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,12 @@
+const square = (num) => {
+    return num * num ;
+  }
+  
+  console.log(square(10));
+
+// const double = (num) => {
+//     return num * 2;
+// }
+
+// // 関数の戻り値を出力する
+// console.log(double(30));
