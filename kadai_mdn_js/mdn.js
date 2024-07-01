b42a8ef74f56c44a3a029694881e761f407e5b6d
Yokereba <kozottio1007@gmail.com>
2024-07-01 11:50:32 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,4 @@
 var today = new Date();
 
 // today.getFullYear();
-console.log (today.getFullYear() + '年' + today.getMonth() + '月'+ today.getDate() + '日') ;
+console.log (today.getFullYear() + '年' + (today.getMonth()+1) + '月'+ today.getDate() + '日') ;
